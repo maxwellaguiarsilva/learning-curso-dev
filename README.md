a7ee8f0987f36bd685a8eb7b074b51a7dd1e6bfb
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2024-11-20 21:38:38 +0000

    adiciona `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # learning-curso-dev
+
 Para finalidade de estudo
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
-
-
-
-export default function Home( )
-{
-  return  <h1>em construção</h1>
+export default function Home() {
+  return <h1>em construção</h1>;
 }
-
-
