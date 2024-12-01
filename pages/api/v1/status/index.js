35b23c1a5b795aaaf577d2dc802fee1f20d27f86
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2024-12-01 18:22:12 +0000

    add `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(request, response) {
+import database from "../../../../infra/database.js";
+
+export default async function status(request, response) {
+  const result = await database.query("select 1+1 as num_total;");
+  console.log(result.rows);
   response.status(200).json({ status: 200 });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
 test("/api/v1/status", async () => {
-  const response = fetch("http://localhost:3000/api/v1/status");
-  expect(1 + 1).toBe(2);
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
 });
