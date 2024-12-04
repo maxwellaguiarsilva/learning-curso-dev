80bbc7a3d2b0ae6b6d1a4890d9d944279ceeb2ce
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2024-12-04 12:40:44 +0000

    add `version` `max_connections` `opened_connections` to `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,32 @@
 import database from "infra/database.js";
 
+const queryVersion = "show server_version;";
+const queryMaxConnections = "show max_connections;";
+
+async function queryFirstRow(queryObject) {
+  return (await database.query(queryObject)).rows.pop();
+}
+
 export default async function status(request, response) {
-  const result = await database.query("select 1+1 as num_total;");
-  console.log(result.rows);
-  response.status(200).json({ status: 200 });
+  const version = (await queryFirstRow(queryVersion)).server_version;
+  const max_connections = (await queryFirstRow(queryMaxConnections))
+    .max_connections;
+
+  const openedConnections = (
+    await queryFirstRow({
+      text: "SELECT count(1)::int as num_total FROM pg_stat_activity WHERE datname=$1",
+      values: [process.env.POSTGRES_DB],
+    })
+  ).num_total;
+
+  response.status(200).json({
+    updated_at: new Date().toISOString(),
+    dependences: {
+      database: {
+        version: version,
+        max_connections: parseInt(max_connections),
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,18 @@
 test("/api/v1/status", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  const result = await response.json();
+
+  expect(result).toEqual({
+    updated_at: new Date(result.updated_at).toISOString(),
+    dependences: {
+      database: {
+        version: "17.2",
+        max_connections: 100,
+        opened_connections: parseInt(
+          result.dependences.database.opened_connections,
+        ),
+      },
+    },
+  });
 });
