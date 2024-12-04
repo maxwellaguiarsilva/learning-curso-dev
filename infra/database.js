8497b5c75e6ab81b2d68d37759ced89a6325c299
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2024-12-04 12:35:30 +0000

    add error handling in `database.js`

--- infra/database.js
+++ infra/database.js
@@ -8,9 +8,17 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
   });
+
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
+  let result;
+  try {
+    result = await client.query(queryObject);
+  } catch (error) {
+    console.error("Error executing query:", error);
+  } finally {
+    await client.end();
+  }
+
   return result;
 }
 
