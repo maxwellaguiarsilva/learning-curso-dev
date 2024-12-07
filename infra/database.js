8a6d3ac3a4df7ce49d4f5174819e6befc0edb255
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2024-12-07 22:30:33 +0000

    make `database.js` ssl compatible

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,23 @@
 import { Client } from "pg";
 
+const connectionData = {
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  ssl: process.env.POSTGRES_SSL_CA
+    ? {
+        rejectUnauthorized: true,
+        ca: process.env.POSTGRES_SSL_CA,
+        key: process.env.POSTGRES_SSL_KEY,
+        cert: process.env.POSTGRES_SSL_CERT,
+      }
+    : false,
+};
+
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-  });
+  const client = new Client(connectionData);
 
   await client.connect();
   let result;
