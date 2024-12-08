ed0b52d7227b6edf1143ec3e7a5b37c2e3887f04
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2024-12-08 16:58:27 +0000

    use `current_database( )` to get `opened_connections`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,6 +2,8 @@ import database from "infra/database.js";
 
 const queryVersion = "show server_version;";
 const queryMaxConnections = "show max_connections;";
+const queryOpenedConnections =
+  "SELECT count(1)::int as opened_connections FROM pg_stat_activity WHERE datname=current_database()";
 
 async function queryFirstRow(queryObject) {
   return (await database.query(queryObject)).rows.pop();
@@ -9,22 +11,18 @@ async function queryFirstRow(queryObject) {
 
 export default async function status(request, response) {
   const version = (await queryFirstRow(queryVersion)).server_version;
-  const max_connections = (await queryFirstRow(queryMaxConnections))
-    .max_connections;
-
-  const openedConnections = (
-    await queryFirstRow({
-      text: "SELECT count(1)::int as num_total FROM pg_stat_activity WHERE datname=$1",
-      values: [process.env.POSTGRES_DB],
-    })
-  ).num_total;
+  const maxConnections = parseInt(
+    (await queryFirstRow(queryMaxConnections)).max_connections,
+  );
+  const openedConnections = (await queryFirstRow(queryOpenedConnections))
+    .opened_connections;
 
   response.status(200).json({
     updated_at: new Date().toISOString(),
     dependences: {
       database: {
         version: version,
-        max_connections: parseInt(max_connections),
+        max_connections: maxConnections,
         opened_connections: openedConnections,
       },
     },
